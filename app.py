96f166311f3247ed3b4a500e2b3791e911f2ca45
Ubuntu <azureuser@openvino.1uquvpxhyvku5lvxp2ra4vyy3d.syx.internal.cloudapp.net>
2024-10-04 05:55:40 +0000

    update: robots.txt added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, send_file
 from dotenv import load_dotenv
 from api.main import api
 load_dotenv()
@@ -13,6 +13,10 @@ app.register_blueprint(api)
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
+
+@app.route('/robots.txt', methods=['GET'])
+def robotstxt():
+    return send_file('static/robots.txt')
 
 @app.errorhandler(Exception)
 def handle_exception(e):
